f7fffdbaca7ce7cabaf45ec49349c881a99dba2a
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-05-02 15:57:30 +0600

    router setup

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Header from '../Header/Header';
+import { Outlet } from 'react-router-dom';
 
 const Main = () => {
     return (
-        <div>
-            <h1>This is main component</h1>
-        </div>
+        <>
+            <Header></Header>
+            <Outlet></Outlet>
+        </>
     );
 };
 
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import {createBrowserRouter,} from "react-router-dom";
 import Main from '../Components/Main/Main';
+import Home from '../Components/Pages/Home/Home';
+import Blogs from '../Components/Pages/Blogs/Blogs';
+import Register from '../Components/Pages/Register/Register';
+import Login from '../Components/Pages/Login/Login';
 
 
 
@@ -10,7 +14,22 @@ const router = createBrowserRouter([
         path: "/",
         element: <Main></Main>,
         children: [
-            
+            {
+                path:'/',
+                element:<Home></Home>
+            },
+            {
+                path:'/blog',
+                element:<Blogs></Blogs>
+            },
+            {
+                path:'login',
+                element: <Login></Login>
+            },
+            {
+                path: 'register',
+                element:<Register></Register>
+            }
         ]
     },
 ]);
