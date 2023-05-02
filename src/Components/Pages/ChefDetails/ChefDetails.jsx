d90f0cd222b28c83574c2a3eb260b2afb2bbd1f8
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-05-03 00:29:40 +0600

    some modify

--- src/Components/Pages/ChefDetails/ChefDetails.jsx
+++ src/Components/Pages/ChefDetails/ChefDetails.jsx
@@ -4,15 +4,16 @@ import { useLoaderData } from 'react-router-dom';
 const ChefDetails = () => {
     const chefDetails = useLoaderData()
     console.log(chefDetails)
-    const { picture, name, num_recipes, years_of_experience, likes, short_bio } = chefDetails
+
+    const { picture, name, num_recipes, years_of_experience, likes, short_bio , recipes } = chefDetails
     return (
         <div className='w-[90%] mx-auto mt-5 '>
 
-            <div className="card card-side shadow-xl  bg-green-100">
+            <div className="card card-side shadow-xl  ">
                 <figure className='w-[60%] h-[500px]'><img className='object-cover' src={picture} alt="Movie" /></figure>
                 <div className=" flex  items-center ml-10 text-start">
                     <div className='flex flex-col gap-3'>
-                        <h2 className="card-title font-bold">{name}</h2>
+                        <h2 className="card-title font-bold text-3xl">{name}</h2>
                         <p>
                             <span className='font-bold'>Years of experience:</span>
                             <span className='text-green-400 font-semibold text-2xl'> {years_of_experience}</span>
@@ -37,7 +38,7 @@ const ChefDetails = () => {
                 </div>
             </div>
             {/* heder end */}
-            
+
 
         </div>
     );
---
+++ src/Components/Pages/ChefDetails/RecipesCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RecipesCard = () => {
+    return (
+        <div>
+            <h1>recipis</h1>
+        </div>
+    );
+};
+
+export default RecipesCard;
