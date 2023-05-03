3dc8d7c8864296d82e4d7073cbf52f9b2c4e7895
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-05-03 15:11:09 +0600

    auth configraration done

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/Routes.jsx'
+import AuthProvider from './provider/AuthProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <AuthProvider>
     <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>,
 )
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, { createContext } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import app from '../firebase/firebase.config';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
-const AuthProvider = () => {
+
+
+export const AuthContext = createContext(null)
+const auth = getAuth(app);
+
+
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+
+
+    const createUser = (email, password) => {
+
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+    const singUser = (email, password) => {
+
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+    const logOut = () => {
+
+        return signOut(auth)
+    }
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, loggedUser => {
+            setUser(loggedUser)
+
+        })
+        return () => {
+            unsubscribe();
+        }
+    }, [])
+    const authInfo = {
+        user,
+        createUser,
+        singUser,
+        logOut,
+    }
+
     return (
-        <div>
-            
-        </div>
-    );
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    )
 };
 
 export default AuthProvider;
